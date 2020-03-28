cc05d7ec84e111fa98d92ef5b686521fc1c50a43
edward <edwardw@qq.com>
2020-03-28 20:47:49 +0800

    init

---
+++ src/hw.js
@@ -0,0 +1,59 @@
+/*
+前缀
+*/
+const targetKeys = ['a', 'a-b', 'a-b-c', 'd', 'd-e-f', 'h-j-k', 'm', 'm-n', 'r-s-t', 'r-s-u', 'r-v']
+
+function count(p){
+  let cnt = 0
+  for (let c of p) {
+    if (c === '-')
+      cnt++
+  }
+  return cnt
+}
+
+targetKeys.sort((a,b) => count(a)-count(b))
+
+console.log(targetKeys)
+/*
+[
+      'a',     'd',
+      'm',   'a-b',
+    'm-n', 'a-b-c',
+  'd-e-f', 'h-j-k',
+  'r-s-t', 'r-s-u'
+]
+*/
+const keys = []
+for (let k of targetKeys) {
+  let flag = 0, s = ''
+  for (let c of k) {
+    s += c
+    if (keys.indexOf(s) !== -1) {
+      flag = 1
+      break;
+    }
+  }
+  if (!flag) {
+    keys.push(k)
+  }
+}
+console.log(keys)
+const res = {}
+for (let path of keys){
+  let node = res
+  for (let step of path.split('-')) {
+    node[step] = node[step] || {}
+    node = node[step]
+  }
+}
+console.log(res)
+/*
+{
+  a: {},
+  d: {},
+  h: { j: { k: {} } },
+  m: {},
+  r: { s: { t: {}, u: {} } }
+}
+*/
