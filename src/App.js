4ba37067ef459322837d0dcc996067f74ca05377
edward <edwardw@qq.com>
2020-04-16 00:40:10 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import { Layout, Row, Col, Table, ConfigProvider, DatePicker, Card, Spin,
+import { Layout, Row, Col, Table, ConfigProvider, DatePicker, Card, Spin, Tree,
   message, Select, Button, Tooltip, Slider, Divider, InputNumber, Checkbox } from 'antd';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import { CheckOutlined, CloseOutlined, RightOutlined, LeftOutlined, SearchOutlined } from '@ant-design/icons';
@@ -22,6 +22,114 @@ import reqwest from 'reqwest';
 
 const count = 3;
 const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat&noinfo`;
+
+const treeData = [
+  {
+    title: '0-0',
+    key: '0-0',
+    children: [
+      {
+        title: '0-0-0',
+        key: '0-0-0',
+        children: [
+          {
+            title: '0-0-0-0',
+            key: '0-0-0-0',
+          },
+          {
+            title: '0-0-0-1',
+            key: '0-0-0-1',
+          },
+          {
+            title: '0-0-0-2',
+            key: '0-0-0-2',
+          },
+        ],
+      },
+      {
+        title: '0-0-1',
+        key: '0-0-1',
+        children: [
+          {
+            title: '0-0-1-0',
+            key: '0-0-1-0',
+          },
+          {
+            title: '0-0-1-1',
+            key: '0-0-1-1',
+          },
+          {
+            title: '0-0-1-2',
+            key: '0-0-1-2',
+          },
+        ],
+      },
+      {
+        title: '0-0-2',
+        key: '0-0-2',
+      },
+    ],
+  },
+  {
+    title: '0-1',
+    key: '0-1',
+    children: [
+      {
+        title: '0-1-0-0',
+        key: '0-1-0-0',
+      },
+      {
+        title: '0-1-0-1',
+        key: '0-1-0-1',
+      },
+      {
+        title: '0-1-0-2',
+        key: '0-1-0-2',
+      },
+    ],
+  },
+  {
+    title: '0-2',
+    key: '0-2',
+  },
+];
+const Tree2 = () => {    
+  const [expandedKeys, setExpandedKeys] = useState(['0-0-0', '0-0-1']);
+  const [checkedKeys, setCheckedKeys] = useState(['0-0-0']);
+  const [selectedKeys, setSelectedKeys] = useState([]);
+  const [autoExpandParent, setAutoExpandParent] = useState(true);
+
+  const onExpand = expandedKeys => {
+    console.log('onExpand', expandedKeys); // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+    // or, you can remove all expanded children keys.
+
+    setExpandedKeys(expandedKeys);
+    setAutoExpandParent(false);
+  };
+
+  const onCheck = checkedKeys => {
+    console.log('onCheck', checkedKeys);
+    setCheckedKeys(checkedKeys);
+  };
+
+  const onSelect = (selectedKeys, info) => {
+    console.log('onSelect', info);
+    setSelectedKeys(selectedKeys);
+  };
+  return (
+    <Tree
+        checkable
+        onExpand={onExpand}
+        expandedKeys={expandedKeys}
+        autoExpandParent={autoExpandParent}
+        onCheck={onCheck}
+        checkedKeys={checkedKeys}
+        onSelect={onSelect}
+        selectedKeys={selectedKeys}
+        treeData={treeData}
+      />
+  )
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -90,6 +198,9 @@ class App extends React.Component {
     const num = items.length;
     const width = (num > 0 ? 100 : 0) + Math.max(0, 96 * (num-1));
     const waitms = 300;
+    const { TreeNode } = Tree;
+
+
     return (
       <div style={{ margin: '0 auto', width: 400}}>
       <Button 
@@ -113,6 +224,10 @@ class App extends React.Component {
 
       <div className="flex-container" style={{ width }}>
         {items}
+      </div>
+      <div>
+      <Tree2/>
+
       </div>
       </div>
     );
